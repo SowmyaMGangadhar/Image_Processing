2f89515285659bed317fb6de4ebe1dd70e0c3473
SowmyaMGangadhar <sowmyamgangadhar@gmail.com>
2024-03-17 10:22:06 +0530

    updating sobel filter for xy

--- Python/sobel_edge_detection.py
+++ Python/sobel_edge_detection.py
@@ -13,22 +13,22 @@ while(True):
     sobel_y = cv2.Sobel(frame, cv2.CV_64F, dx=0, dy=1, ksize=3)
     sobel_xy = cv2.Sobel(frame, cv2.CV_64F, dx=1, dy=1, ksize=5)
 
-    save_img = "/Users/sowmyamgangadhar/Documents/ComputerVision/2D/Assignments/Image_Processing/results/"
-    os.makedirs(save_img, exist_ok=True)
+    # save_img = "/ComputerVision/2D/Assignments/Image_Processing/results/"
+    # os.makedirs(save_img, exist_ok=True)
 
-    output_path1 = os.path.join(save_img, "sobel_x.jpg")
-    output_path2 = os.path.join(save_img, "sobel_y.jpg")
-    output_path3 = os.path.join(save_img, "sobel_xy.jpg")
-    cv2.imwrite(output_path1, sobel_x)
-    cv2.imwrite(output_path2, sobel_y)
-    cv2.imwrite(output_path3, sobel_xy)
+    # output_path1 = os.path.join(save_img, "sobel_x.jpg")
+    # output_path2 = os.path.join(save_img, "sobel_y.jpg")
+    # output_path3 = os.path.join(save_img, "sobel_xy.jpg")
+    # cv2.imwrite(output_path1, sobel_x)
+    # cv2.imwrite(output_path2, sobel_y)
+    # cv2.imwrite(output_path3, sobel_xy)
 
 
    
 
-#     cv2.imshow("sobel", sobel_xy)
+    cv2.imshow("sobel", sobel_xy)
 
-#     if cv2.waitKey(1)& 0xFF==ord('q'):
-#         break
-# video.release()
-# cv2.destroyAllWindows()
+    if cv2.waitKey(1)& 0xFF==ord('q'):
+        break
+video.release()
+cv2.destroyAllWindows()
