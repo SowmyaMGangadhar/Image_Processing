ee3cd85b386e6da4f3fd641410f69a76a3021669
SowmyaMGangadhar <sowmyamgangadhar@gmail.com>
2024-03-17 09:41:56 +0530

    updating Sobel operator

--- Python/Sobel_Filter_Scratch.py
+++ Python/Sobel_Filter_Scratch.py
@@ -3,11 +3,10 @@ import numpy as np
 import os
 
 def sobel_filter(img):
-
     grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     sobel_x = np.array([[-1, 0, 1],[-2, 0, 2],[-1, 0, 1]])
     sobel_y = np.array([[1, 2, 1],[0, 0, 0],[-1, -2, -1]])
-    print(sobel_x, sobel_y)
+    # print(sobel_x, sobel_y)
 
     [row, col] = np.shape(grey)
     print(row, col)
@@ -27,10 +26,12 @@ def sobel_filter(img):
             gy = np.sum(np.multiply(sobel_y, grey[i:i+3, j:j+3]))
             # print(gx,gy)
             sob[i+1, j+1] = np.sqrt(gx**2 + gy**2)
+            # print(sob)
+            
     sob = cv2.normalize(sob, None, 0, 255, cv2.NORM_MINMAX)
     sob = sob.astype(np.uint8)
 
-    save_img = "/Users/sowmyamgangadhar/Documents/ComputerVision/2D/Assignments/Image_Processing/results/"
+    save_img = "/ComputerVision/2D/Assignments/Image_Processing/results/"
     os.makedirs(save_img, exist_ok=True)
 
     output_path = os.path.join(save_img, "sobel_scartch.jpg")
@@ -41,5 +42,5 @@ def sobel_filter(img):
     cv2.destroyAllWindows()
 
 
-img = cv2.imread('/Users/sowmyamgangadhar/Documents/ComputerVision/2D/Assignments/Image_Processing/dataset/sunflower1.jpg')
+img = cv2.imread('/ComputerVision/2D/Assignments/Image_Processing/dataset/sunflower1.jpg')
 sobel_filter(img)
