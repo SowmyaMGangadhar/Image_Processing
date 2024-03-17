8926dd6918c5c961fe7e4bf973f9a33f08fff8dd
SowmyaMGangadhar <sowmyamgangadhar@gmail.com>
2024-03-17 09:10:24 +0530

    updating Sobel operator

--- Python/Sobel_Fileter_Scratch.py
+++ Python/Sobel_Fileter_Scratch.py
@@ -3,9 +3,8 @@ import numpy as np
 import os
 
 def sobel_filter(img):
-    # img = cv2.imread('/Users/sowmyamgangadhar/Documents/ComputerVision/2D/Assignments/Image_Processing/dataset/sunflower1.jpg')
-    grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+    grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     sobel_x = np.array([[-1, 0, 1],[-2, 0, 2],[-1, 0, 1]])
     sobel_y = np.array([[1, 2, 1],[0, 0, 0],[-1, -2, -1]])
     print(sobel_x, sobel_y)
@@ -13,6 +12,15 @@ def sobel_filter(img):
     [row, col] = np.shape(grey)
     print(row, col)
     sob = np.zeros(shape=(row, col), dtype=np.float64)
+    
+    # array = np.array([[1,2,3,4,5,6,7,8,9,0],[0,9,8,7,6,5,4,3,2,1],[2,1,3,4,2,5,67,8,9,2], [77,2,211,55,77,88,99,23,66,73]])
+    # print(array[0:0+3, 0:0+3])
+    # print(grey[0:0+3,0:0+3])
+    # gx = np.sum(np.multiply(sobel_x, grey[1:1+3, 1:1+3]))
+    # gy = np.sum(np.multiply(sobel_y, grey[1:1+3, 1:1+3]))
+    # print(gr,gy)
+    
+    
     for i in range(row-2):
         for j in range(col-2):
             gx = np.sum(np.multiply(sobel_x, grey[i:i+3, j:j+3]))
