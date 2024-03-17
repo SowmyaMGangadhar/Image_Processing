bb25a2c1304b20211041bcb6e6d4d5ecf8f5d57b
SowmyaMGangadhar <sowmyamgangadhar@gmail.com>
2024-03-17 10:18:22 +0530

    adding modified sobel operator and updating outputs

--- Python/sobel_edge_detection.py
+++ Python/sobel_edge_detection.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import os
 
 video = cv2.VideoCapture(0)
 
@@ -8,15 +9,26 @@ while(True):
     
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
-    sobel_x = cv2.Sobel(frame, cv2.CV_64F, 1, 0, ksize=3)
-    sobel_y = cv2.Sobel(frame, cv2.CV_64F, 0, 1, ksize=3)
+    sobel_x = cv2.Sobel(frame, cv2.CV_64F, dx=1, dy=0, ksize=3)
+    sobel_y = cv2.Sobel(frame, cv2.CV_64F, dx=0, dy=1, ksize=3)
+    sobel_xy = cv2.Sobel(frame, cv2.CV_64F, dx=1, dy=1, ksize=5)
 
-    sobel = np.hypot(sobel_x, sobel_y)
-    sobel = (sobel/sobel.max())
+    save_img = "/Users/sowmyamgangadhar/Documents/ComputerVision/2D/Assignments/Image_Processing/results/"
+    os.makedirs(save_img, exist_ok=True)
 
-    cv2.imshow("sobel", sobel)
+    output_path1 = os.path.join(save_img, "sobel_x.jpg")
+    output_path2 = os.path.join(save_img, "sobel_y.jpg")
+    output_path3 = os.path.join(save_img, "sobel_xy.jpg")
+    cv2.imwrite(output_path1, sobel_x)
+    cv2.imwrite(output_path2, sobel_y)
+    cv2.imwrite(output_path3, sobel_xy)
 
-    if cv2.waitKey(1)& 0xFF==ord('q'):
-        break
-video.release()
-cv2.destroyAllWindows()
+
+   
+
+#     cv2.imshow("sobel", sobel_xy)
+
+#     if cv2.waitKey(1)& 0xFF==ord('q'):
+#         break
+# video.release()
+# cv2.destroyAllWindows()
